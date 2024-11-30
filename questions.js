a4b27b285d7f5cf0783e7014a20b23de49ad8b2e
JESSE133h4 <162440817+JESSE133h4@users.noreply.github.com>
2024-11-30 00:43:47 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    question: "Quem foi Daemon Targaryen?",
+    answers: [
+      { option: "Um rei de Westeros", correct: false },
+      { option: "Um Príncipe e guerreiro Targaryen", correct: true },
+      { option: "Um Dragão", correct: false },
+    ],
+  },
+  {
+    question: "Qual dragão Daemon Targaryen montava?",
+    answers: [
+      { option: "Vhagar", correct: false },
+      { option: "Caraxes", correct: true },
+      { option: "Syrax", correct: false },
+    ],
+  },
+  {
+    question: "Qual era o título de Daemon Targaryen na Guarda da Cidade?",
+    answers: [
+      { option: "Senhor Comandante", correct: true },
+      { option: "Mestre dos Sussurros", correct: false },
+      { option: "Mestre das Moedas", correct: false },
+    ],
+  },
+  {
+    question: "Qual batalha Daemon Targaryen notoriamente lutou?",
+    answers: [
+      { option: "A Batalha da Água Negra", correct: false },
+      { option: "A Batalha da Goela", correct: true },
+      { option: "A Batalha dos Bastardos", correct: false },
+    ],
+  },
+  {
+    question: "Quem era o irmão de Daemon Targaryen?",
+    answers: [
+      { option: "Viserys I Targaryen", correct: true },
+      { option: "Aegon II Targaryen", correct: false },
+      { option: "Rhaegar Targaryen", correct: false },
+    ],
+  },
+  {
+    question: "Qual era a cor das escamas do dragão Caraxes?",
+    answers: [
+      { option: "Verde", correct: false },
+      { option: "Vermelha", correct: true },
+      { option: "Preta", correct: false },
+    ],
+  },
+  {
+    question: "Quem era a esposa de Daemon Targaryen?",
+    answers: [
+      { option: "Rhaenyra Targaryen", correct: true },
+      { option: "Alicent Hightower", correct: false },
+      { option: "Lyanna Stark", correct: false },
+    ],
+  },
+  {
+    question: "Em que cidade Daemon Targaryen serviu como Príncipe da Muralha?",
+    answers: [
+      { option: "Pentos", correct: false },
+      { option: "Ghis", correct: false },
+      { option: "Quarto de Pulgas", correct: true },
+    ],
+  },
+];
